811bbbb205bd5e6aee730bbf464300f9ed8a2e10
Chale Sithole <sitholechale3@gmail.com>
2026-07-09 11:53:54 +0200

    Configure Entity Framework Core and create initial database

---
+++ StudentManagementSystem/StudentManagementSystem/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementSystem.Models
+{
+    public class Student
+    {
+        [Key]
+        [Required]
+        [StringLength(15)]
+        public string StudentNumber { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Phone]
+        public string? Phone { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Programme { get; set; } = string.Empty;
+
+        [Range(1, 6)]
+        public int YearLevel { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
