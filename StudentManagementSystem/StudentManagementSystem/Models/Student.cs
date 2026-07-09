689a55f694a72bbad1a276553f20c0b1d3dda707
Chale Sithole <sitholechale3@gmail.com>
2026-07-09 11:58:12 +0200

    Configure Entity Framework Core and create initial database

--- StudentManagementSystem/StudentManagementSystem/Models/Student.cs
+++ StudentManagementSystem/StudentManagementSystem/Models/Student.cs
@@ -1,39 +1,36 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace StudentManagementSystem.Models
+public class Student
 {
-    public class Student
-    {
-        [Key]
-        [Required]
-        [StringLength(15)]
-        public string StudentNumber { get; set; } = string.Empty;
+    [Key]
+    [Required]
+    [StringLength(15)]
+    public string StudentNumber { get; set; } = string.Empty;
 
-        [Required]
-        [StringLength(50)]
-        public string FirstName { get; set; } = string.Empty;
+    [Required]
+    [StringLength(50)]
+    public string FirstName { get; set; } = string.Empty;
 
-        [Required]
-        [StringLength(50)]
-        public string LastName { get; set; } = string.Empty;
+    [Required]
+    [StringLength(50)]
+    public string LastName { get; set; } = string.Empty;
 
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; } = string.Empty;
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
 
-        [Phone]
-        public string? Phone { get; set; }
+    public string? Phone { get; set; }
 
-        [DataType(DataType.Date)]
-        public DateTime DateOfBirth { get; set; }
+    public DateTime DateOfBirth { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string Programme { get; set; } = string.Empty;
+    [Required]
+    public string Faculty { get; set; } = string.Empty;
 
-        [Range(1, 6)]
-        public int YearLevel { get; set; }
+    [Required]
+    public string Programme { get; set; } = string.Empty;
 
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-    }
+    [Range(1, 6)]
+    public int YearLevel { get; set; }
+
+    public DateTime RegistrationDate { get; set; } = DateTime.Now;
 }
